09b68687198af7ef88f179a74882ec51ad08fdc3
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-07-27 07:34:00 -0700

    Add files via upload

---
+++ Python Advanced/Lecture 8/recursive_power.py
@@ -0,0 +1,8 @@
+def recursive_power(num, power):
+    if power == 0:
+        return 1
+    return num * recursive_power(num, power - 1)
+
+
+print(recursive_power(2, 100))
+print(recursive_power(10, 100))
