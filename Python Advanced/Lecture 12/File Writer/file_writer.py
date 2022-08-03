abc7b412ab1d7f75a793aadb4c16e9224e3137a4
Ivailo Nikolov <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-08-03 04:44:42 -0700

    Add files via upload

---
+++ Python Advanced/Lecture 12/File Writer/file_writer.py
@@ -0,0 +1,6 @@
+with open("my_first_file.txt", "a") as file:
+    file.write("I just created my first file!\n")
+
+with open("my_first_file.txt", "a") as file:
+    file.write("\nI just created my second file!")
+
