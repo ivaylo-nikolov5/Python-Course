9cbe70a4a032417aaaf8aa657b5fa4449ebed305
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-08-19 10:36:55 +0300

    Added a solution for the Pokemon problem

--- Python OOP/Lecture 2/project/trainer.py
+++ Python OOP/Lecture 2/project/trainer.py
@@ -1,6 +1,4 @@
 from pokemon import Pokemon
-
-
 class Trainer:
     def __init__(self, name):
         self.name = name
@@ -26,13 +24,3 @@ class Trainer:
 
         return result
 
-pokemon = Pokemon("Pikachu", 90)
-print(pokemon.pokemon_details())
-trainer = Trainer("Ash")
-print(trainer.add_pokemon(pokemon))
-second_pokemon = Pokemon("Charizard", 110)
-print(trainer.add_pokemon(second_pokemon))
-print(trainer.add_pokemon(second_pokemon))
-print(trainer.release_pokemon("Pikachu"))
-print(trainer.release_pokemon("Pikachu"))
-print(trainer.trainer_data())
