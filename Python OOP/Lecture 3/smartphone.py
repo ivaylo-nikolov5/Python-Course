bbbd62670582ec0b9853f2aa1f6affa10a414602
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-08-21 06:48:34 +0300

    Revised the lecture and fixed one of the problems' solution

--- Python OOP/Lecture 3/smartphone.py
+++ Python OOP/Lecture 3/smartphone.py
@@ -5,20 +5,17 @@ class Smartphone:
         self.is_on = False
 
     def power(self):
-        if not self.is_on:
-            self.is_on = True
-        else:
-            self.is_on = False
+        self.is_on = not self.is_on
 
     def install(self, app, app_memory):
         if self.memory >= app_memory and self.is_on:
-            self.memory -= app_memory
             self.apps.append(app)
+            self.memory -= app_memory
             return f"Installing {app}"
         elif not self.is_on:
             return f"Turn on your phone to install {app}"
-        else:
-            return f"Not enough memory to install {app}"
+
+        return f"Not enough memory to install {app}"
 
     def status(self):
         return f"Total apps: {len(self.apps)}. Memory left: {self.memory}"
