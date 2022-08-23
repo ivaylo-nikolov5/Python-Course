cd8849f10ccb686e36f4c2286c6fcb62b4044ce6
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-08-23 18:08:20 +0300

    Resolved the problems from the lecture

---
+++ Python OOP/Lecture 4/project/library.py
@@ -0,0 +1,31 @@
+from project.user import User
+
+
+class Library:
+    def __init__(self):
+        self.user_records = []
+        self.books_available = {}
+        self.rented_books = {}
+
+    def get_book(self, author: str, book_name: str, days_to_return: int, user: User):
+        for author_name, books in self.books_available.items():
+            if author == author_name and book_name in books:
+                user.books.append(book_name)
+                self.books_available[author_name].remove(book_name)
+                if user.username not in self.rented_books:
+                    self.rented_books[user.username] = {}
+                self.rented_books[user.username][book_name] = days_to_return
+                return f"{book_name} successfully rented for the next {days_to_return} days!"
+        book_info = self.rented_books[user.username][book_name]
+        return f'The book "{book_name}" is already rented and will be available in {book_info} days!'
+
+    def return_book(self, author:str, book_name:str, user: User):
+        for book in user.books:
+            if book == book_name:
+                user.books.remove(book)
+                self.books_available[author].append(book_name)
+                del self.rented_books[user.username][book_name]
+                break
+        else:
+            return f"{user.username} doesn't have this book in his/her records!"
+
