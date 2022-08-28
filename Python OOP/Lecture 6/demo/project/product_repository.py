feddf9fd0856531cbcfe1d74d179d57c3c5f7ec2
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-08-28 12:13:21 +0300

    Resolving problems for yesterday

---
+++ Python OOP/Lecture 6/demo/project/product_repository.py
@@ -0,0 +1,26 @@
+from project.product import Product
+
+
+class ProductRepository:
+    def __init__(self):
+        self.products = []
+
+    def add(self, product: Product):
+        self.products.append(product)
+
+    def find(self, product_name: str):
+        for product in self.products:
+            if product.name == product_name:
+                return product
+
+    def remove(self, product_name):
+        if self.find(product_name) is not None:
+            self.products.remove(self.find(product_name))
+
+    def __repr__(self):
+        result = ""
+        for product in self.products:
+            result += f"{product.name}: {product.quantity}\n"
+
+        return result.strip()
+
