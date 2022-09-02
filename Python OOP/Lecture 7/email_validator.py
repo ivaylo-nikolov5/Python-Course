8d632edf0a9758c9c4ad567495558ab82d48c25b
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-02 10:46:14 +0300

    Added a solution for the Email Validator problem

---
+++ Python OOP/Lecture 7/email_validator.py
@@ -0,0 +1,33 @@
+import re
+
+
+class EmailValidator:
+    def __init__(self, min_length, mails, domains):
+        self.min_length = min_length
+        self.mails = mails
+        self.domains = domains
+
+    def __is_name_valid(self, name):
+        return len(name) >= self.min_length
+
+    def __is_mail_valid(self, mail):
+        return mail in self.mails
+
+    def __is_domain_valid(self, domain):
+        return domain in self.domains
+
+    def validate(self, email):
+        email_parts = re.match(r"([a-zA-Z0-9]+)@([a-z]+).([a-z]+)", email)
+        username = email_parts[1]
+        mail = email_parts[2]
+        domain = email_parts[3]
+        return self.__is_name_valid(username) and self.__is_mail_valid(mail) and self.__is_domain_valid(domain)
+
+
+mails = ["gmail", "softuni"]
+domains = ["com", "bg"]
+email_validator = EmailValidator(6, mails, domains)
+print(email_validator.validate("pe77er@gmail.com"))
+print(email_validator.validate("georgios@gmail.net"))
+print(email_validator.validate("stamatito@abv.net"))
+print(email_validator.validate("abv@softuni.bg"))
