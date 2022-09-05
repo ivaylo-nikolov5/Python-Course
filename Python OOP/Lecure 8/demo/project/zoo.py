13acc5bfb820002c2634a8966e6a7f7e04126d0d
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-05 09:01:56 +0300

    Resolved the Wild Cat Zoo problem

--- Python OOP/Lecure 8/demo/project/zoo.py
+++ Python OOP/Lecure 8/demo/project/zoo.py
@@ -1,51 +1,53 @@
+from project.animal import Animal
+from project.worker import Worker
+
+
 class Zoo:
     def __init__(self, name, budget, animal_capacity, workers_capacity):
         self.name = name
+        self.animals = []
+        self.workers = []
         self.__budget = budget
         self.__animal_capacity = animal_capacity
         self.__workers_capacity = workers_capacity
-        self.animals = []
-        self.workers = []
 
-    def add_animal(self, animal, price):
-        if self.__budget >= price and len(self.animals) < self.__animal_capacity:
-            self.animals.append(animal)
-            self.__budget -= price
-            return f"{animal.name} the {animal.__class__.__name__} added to the zoo"
-        elif self.__budget < price:
+    def add_animal(self, animal: Animal, price):
+        if self.__budget < price:
             return "Not enough budget"
-        return "Not enough space for animal"
+        elif len(self.animals) == self.__animal_capacity:
+            return "Not enough space for animal"
+        self.animals.append(animal)
+        self.__budget -= price
+        return f"{animal.name} the {animal.__class__.__name__} added to the zoo"
 
-    def hire_worker(self, worker):
-        if len(self.workers) < self.__workers_capacity:
-            self.workers.append(worker)
-            return f"{worker.name} the {worker.__class__.__name__} hired successfully"
-        return "Not enough space for worker"
+    def hire_worker(self, worker: Worker):
+        if len(self.workers) == self.__workers_capacity:
+            return "Not enough space for worker"
+
+        self.workers.append(worker)
+        return f"{worker.name} the {worker.__class__.__name__} hired successfully"
 
     def fire_worker(self, worker_name):
         for worker in self.workers:
             if worker.name == worker_name:
                 self.workers.remove(worker)
                 return f"{worker_name} fired successfully"
+
         return f"There is no {worker_name} in the zoo"
 
     def pay_workers(self):
-        to_pay = 0
-        for worker in self.workers:
-            to_pay += worker.salary
-
+        to_pay = sum([worker.salary for worker in self.workers])
         if to_pay > self.__budget:
-            return f"You have no budget to pay your workers. They are unhappy"
+            return "You have no budget to pay your workers. They are unhappy"
+
         self.__budget -= to_pay
         return f"You payed your workers. They are happy. Budget left: {self.__budget}"
 
     def tend_animals(self):
-        to_tend = 0
-        for animal in self.animals:
-            to_tend += animal.money_for_care
-
+        to_tend = sum(animal.money_for_care for animal in self.animals)
         if to_tend > self.__budget:
-            return f"You have no budget to tend the animals. They are unhappy."
+            return "You have no budget to tend the animals. They are unhappy."
+
         self.__budget -= to_tend
         return f"You tended all the animals. They are happy. Budget left: {self.__budget}"
 
@@ -80,3 +82,4 @@ class Zoo:
             result += f"\n{repr(entity)}"
 
         return result
+
