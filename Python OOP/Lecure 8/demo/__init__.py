8f26ca825d60d601f7f4e8da3132552061d6d12e
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-05 10:57:47 +0300

    Resolved the Football Team Generator problem

--- Python OOP/Lecure 8/demo/__init__.py
+++ Python OOP/Lecure 8/demo/__init__.py
@@ -1,40 +1,26 @@
-from project.dough import Dough
-from project.pizza import Pizza
-from project.topping import Topping
+from project.player import Player
+from project.team import Team
 
 
-tomato_topping = Topping("Tomato", 60)
-print(tomato_topping.topping_type)
-print(tomato_topping.weight)
+p = Player("Pall", 1, 3, 5, 7)
 
-mushrooms_topping = Topping("Mushroom", 75)
-print(mushrooms_topping.topping_type)
-print(mushrooms_topping.weight)
+print("Player name:", p.name)
+print("Points sprint:", p._Player__sprint)
+print("Points dribble:", p._Player__dribble)
+print("Points passing:", p._Player__passing)
+print("Points shooting:", p._Player__shooting)
 
-mozzarella_topping = Topping("Mozzarella", 80)
-print(mozzarella_topping.topping_type)
-print(mozzarella_topping.weight)
+print("\ncalling the __str__ method")
+print(p)
 
-cheddar_topping = Topping("Cheddar", 150)
+print("\nAbout the team")
+t = Team("Best", 10)
+print("Team name:", t._Team__name)
+print("Teams points:", t._Team__rating)
+print("Teams players:", len(t._Team__players))
+print(t.add_player(p))
+print(t.add_player(p))
+print("Teams players:", len(t._Team__players))
+print(t.remove_player("Pall"))
+print(t.remove_player("Pall"))
 
-pepperoni_topping = Topping("Pepperoni", 120)
-
-white_flour_dough = Dough("White Flour", "Mixing", 200)
-print(white_flour_dough.flour_type)
-print(white_flour_dough.weight)
-print(white_flour_dough.baking_technique)
-
-whole_wheat_dough = Dough("Whole Wheat Flour", "Mixing", 200)
-print(whole_wheat_dough.weight)
-print(whole_wheat_dough.flour_type)
-print(whole_wheat_dough.baking_technique)
-
-p = Pizza("Margherita", whole_wheat_dough, 2)
-
-p.add_topping(tomato_topping)
-print(p.calculate_total_weight())
-
-p.add_topping(mozzarella_topping)
-print(p.calculate_total_weight())
-
-p.add_topping(mozzarella_topping)
