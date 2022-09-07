12f155f74967db9a556c55687f2146149bb99b66
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-07 07:49:58 +0300

    Resolved the Shop problem

--- Python OOP/Lecture 9/demo.py
+++ Python OOP/Lecture 9/demo.py
@@ -1,28 +1,42 @@
-from functools import reduce
+class Shop:
+    def __init__(self, name, type, capacity):
+        self.name = name
+        self.type = type
+        self.capacity = capacity
+        self.items = {}
+
+    @classmethod
+    def small_shop(cls, name: str, type: str):
+        return cls(name, type, 10)
+
+    def add_item(self, item_name: str):
+        if sum(self.items.values()) == self.capacity:
+            return "Not enough capacity in the shop"
+        elif item_name not in self.items:
+            self.items[item_name] = 0
+        self.items[item_name] += 1
+        return f"{item_name} added to the shop"
+
+    def remove_item(self, item_name: str, amount: int):
+        if item_name not in self.items or amount > self.items[item_name]:
+            return f"Cannot remove {amount} {item_name}"
+        self.items[item_name] -= amount
+        if self.items[item_name] == 0:
+            del self.items[item_name]
+        return f"{amount} {item_name} removed from the shop"
+
+    def __repr__(self):
+        return f"{self.name} of type {self.type} with capacity {self.capacity}"
 
 
-class Calculator:
-    def __init__(self):
-        pass
+fresh_shop = Shop("Fresh Shop", "Fruit and Veg", 50)
+small_shop = Shop.small_shop("Fashion Boutique", "Clothes")
+print(fresh_shop)
+print(small_shop)
 
-    @staticmethod
-    def add(*args):
-        return reduce(lambda x, y: x + y, args)
+print(fresh_shop.add_item("Bananas"))
+print(fresh_shop.remove_item("Tomatoes", 2))
 
-    @staticmethod
-    def multiply(*args):
-        return reduce(lambda x, y: x * y, args)
-
-    @staticmethod
-    def divide(*args):
-        return reduce(lambda x, y: x / y, args)
-
-    @staticmethod
-    def subtract(*args):
-        return reduce(lambda x, y: x - y, args)
-
-
-print(Calculator.add(5, 10, 4))
-print(Calculator.multiply(1, 2, 3, 5))
-print(Calculator.divide(100, 2))
-print(Calculator.subtract(90, 20, -50, 43, 7))
+print(small_shop.add_item("Jeans"))
+print(small_shop.add_item("Jeans"))
+print(small_shop.remove_item("Jeans", 2))
