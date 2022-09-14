f42fa86e5510dc21b3c33a9671d7a7ea23ff6595
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-14 13:37:27 +0300

    Revised the Animals problem

--- Python OOP/Lecture 12/demo.py
+++ Python OOP/Lecture 12/demo.py
@@ -1,14 +1,11 @@
-from wild_farm.project.animals.birds import Hen
-from wild_farm.project.food import Meat, Vegetable, Fruit
+from project.cat import Cat
+from project.dog import Dog
+from project.kitten import Kitten
+from project.tomcat import Tomcat
 
-hen = Hen("Harry", 10, 10)
-veg = Vegetable(3)
-fruit = Fruit(5)
-meat = Meat(1)
-print(hen)
-print(hen.make_sound())
-hen.feed(veg)
-hen.feed(fruit)
-hen.feed(meat)
-print(hen)
-
+kitten = Kitten("Kiki", 1)
+print(kitten.make_sound())
+print(kitten)
+cat = Cat("Johnny", 7, "Male")
+print(cat.make_sound())
+print(cat)
---
+++ Python OOP/Lecture 12/project/animal.py
@@ -0,0 +1,14 @@
+from abc import ABC
+
+
+class Animal(ABC):
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def make_sound(self):
+        pass
+
+    def __repr__(self):
+        return f"This is {self.name}. {self.name} is a {self.age} year old {self.gender} {self.__class__.__name__}"
