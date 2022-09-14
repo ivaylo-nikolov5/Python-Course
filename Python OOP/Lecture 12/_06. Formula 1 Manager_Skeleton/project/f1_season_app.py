14a1eb1bd96b87e905dacde7b985ada8e16b9a70
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-14 14:48:13 +0300

    Added calculate revenue after race function

---
+++ Python OOP/Lecture 12/_06. Formula 1 Manager_Skeleton/project/f1_season_app.py
@@ -0,0 +1,18 @@
+class FormulaTeam:
+    MINIMUM_BUDGET = 1000000
+
+    def __init__(self, budget):
+        self.budget = budget
+        
+    @property
+    def budget(self):
+        return self.__budget
+    
+    @budget.setter
+    def budget(self, value):
+        if value < FormulaTeam.MINIMUM_BUDGET:
+            raise ValueError("F1 is an expensive sport, find more sponsors!")
+        self.__budget = value
+
+    def calculate_revenue_after_race(self, race_pos: int):
+        pass
