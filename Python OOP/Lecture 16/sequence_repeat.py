072caea0bae3e95b663884dcb02bf9bcd250501a
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-18 17:58:00 +0300

    Added a solution for the Sequence Repeat problem

---
+++ Python OOP/Lecture 16/sequence_repeat.py
@@ -0,0 +1,24 @@
+class sequence_repeat:
+    def __init__(self, sequence, number):
+        self.sequence = sequence
+        self.number = number
+        self.idx = 0
+        self.passed_iterations = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.passed_iterations == self.number:
+            raise StopIteration
+        elif self.idx % len(self.sequence) == 0:
+            self.idx = 0
+        current = self.sequence[self.idx]
+        self.passed_iterations += 1
+        self.idx += 1
+        return current
+
+result = sequence_repeat('I Love Python', 3)
+for item in result:
+    print(item, end ='')
+
