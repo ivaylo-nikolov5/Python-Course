eceef6aad4ce301c20593b6672188b21e5e9e7a8
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-18 16:54:38 +0300

    Added a solution for the Dictionary Iterator problem

--- Python OOP/Lecture 16/demo.py
+++ Python OOP/Lecture 16/demo.py
@@ -1,4 +1,2 @@
-d = {"a": "a", "b": "b"}
 
-keys = list(d.keys())
-print(keys)
+
--- Python OOP/Lecture 16/dictionary_iterator.py
+++ Python OOP/Lecture 16/dictionary_iterator.py
@@ -1,21 +1,19 @@
 class dictionary_iter:
     def __init__(self, obj: dict):
         self.obj = obj
-        self.key_idx = 0
+        self.idx = 0
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.key_idx == len(self.obj):
+        if self.idx == len(self.obj):
             raise StopIteration
-        keys = list(self.obj.keys())
-        values = list(self.obj.values())
-        result = (keys[self.key_idx], values[self.key_idx])
-        self.key_idx += 1
+        kvpt = list(self.obj.items())
+        result = kvpt[self.idx]
+        self.idx += 1
         return result
 
 result = dictionary_iter({"name": "Peter", "age": 24})
 for x in result:
     print(x)
-
