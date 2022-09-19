6013181b6ee8c5e1bd12bf0ebf3e752f150fe59a
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-19 18:40:14 +0300

    Revised the Reader problem

--- Python OOP/Lecture 16/demo.py
+++ Python OOP/Lecture 16/demo.py
@@ -1,16 +1,9 @@
-def fibonacci():
-    num1 = 0
-    num2 = 1
-    yield num1
-    yield num2
-    while True:
-        next_num = num1 + num2
-        yield next_num
-        num1 = num2
-        num2 = next_num
+def read_next(*args):
+    for obj in args:
+        for i in obj:
+            yield i
 
 
-generator = fibonacci()
-for i in range(1):
-    print(next(generator))
+for i in read_next("Need", (2, 3), ["words", "."]):
+    print(i)
 
