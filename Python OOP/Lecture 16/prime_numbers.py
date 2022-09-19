73a2fdf777df4d2ee8c5aaa432d477afb2904fb5
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-19 14:16:34 +0300

    Added a solution for the Prime Numbers problem

--- Python OOP/Lecture 16/prime_numbers.py
+++ Python OOP/Lecture 16/prime_numbers.py
@@ -1,16 +1,21 @@
+def is_prime(num):
+    flag = False
+    if num <= 1:
+        flag = True
+
+    for i in range(2, num):
+        if num % i == 0:
+            flag = True
+            break
+
+    if not flag:
+        return True
+
+
 def get_primes(nums):
     for num in nums:
-        if num <= 1:
-            continue
-        flag = False
-
-        for i in range(2, num):
-            if num % i == 0:
-                flag = True
-                break
-
-        if not flag:
+        if is_prime(num):
             yield num
 
 
-print(list(get_primes([-2, 0, 0, 1, 1, 0])))
+print(list(get_primes([2, 4, 3, 5, 6, 9, 1, 0])))
