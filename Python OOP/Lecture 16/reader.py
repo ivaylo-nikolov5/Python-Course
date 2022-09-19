beb55d445edc42a571d5d6e126d821320900eae2
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-19 13:59:31 +0300

    Added a solution for the Reader problem

--- Python OOP/Lecture 16/reader.py
+++ Python OOP/Lecture 16/reader.py
@@ -10,8 +10,8 @@ def read_next(*args):
         idx += 1
 
 
-# for item in read_next("string", (2,), {"d": 1, "i": 2, "c": 3, "t": 4}):
-#     print(item, end='')
+for item in read_next("string", (2,), {"d": 1, "i": 2, "c": 3, "t": 4}):
+    print(item, end='')
 
 for i in read_next("Need", (2, 3), ["words", "."]):
     print(i)
