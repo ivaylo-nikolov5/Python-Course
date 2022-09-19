c12a94619ab20dd63c231a4e3f540a8a5c2906bd
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-19 15:15:14 +0300

    Added a solution for the Possible Permutations problem

--- Python OOP/Lecture 16/possible_permutations.py
+++ Python OOP/Lecture 16/possible_permutations.py
@@ -1,7 +1,9 @@
 from itertools import permutations
 
+
 def possible_permutations(nums):
     for perm in permutations(nums):
         yield list(perm)
+
 
 [print(n) for n in possible_permutations([1])]
