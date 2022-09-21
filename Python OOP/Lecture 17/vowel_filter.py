59fef375a3d844aa2a81f5e4fe68fff5f9a5480d
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-21 14:23:45 +0300

    Added a solution for the Vowel Filter problem

---
+++ Python OOP/Lecture 17/vowel_filter.py
@@ -0,0 +1,19 @@
+def vowel_filter(function):
+
+    def wrapper():
+        letters = function()
+        result = []
+        for letter in letters:
+            if letter in "aeuioy":
+                result.append(letter)
+
+        return result
+
+    return wrapper
+
+
+@vowel_filter
+def get_letters():
+    return ["a", "b", "c", "d", "e"]
+
+print(get_letters())
