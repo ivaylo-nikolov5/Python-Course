ac6d7003e302e24fd8d182ab9d757ef611ce0619
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-23 16:57:32 +0300

    Added a solution for the Store Results problem

---
+++ Python OOP/Lecture 18/store_results.py
@@ -0,0 +1,19 @@
+class store_results:
+    def __init__(self, function):
+        self.function = function
+
+    def __call__(self, *args):
+        with open("results.txt", "a") as file:
+            file.writelines(f"Function '{self.function.__name__}' was called. Result: {self.function(*args)}\n")
+
+
+@store_results
+def add(a, b):
+    return a + b
+
+@store_results
+def mult(a, b):
+    return a * b
+
+add(2, 2)
+mult(6, 4)
