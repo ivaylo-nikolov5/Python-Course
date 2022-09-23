419fd9a65b8023f49bee6080492da3a7e5fbb62b
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-09-23 16:38:28 +0300

    Added a solution for the Cache problem

---
+++ Python OOP/Lecture 18/cache.py
@@ -0,0 +1,23 @@
+def cache(function):
+    log = {}
+
+    def wrapper(number):
+        if number in log:
+            return log[number]
+        result = function(number)
+        log[number] = result
+        return result
+
+    wrapper.log = log
+    return wrapper
+
+
+@cache
+def fibonacci(n):
+    if n < 2:
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+fibonacci(3)
+print(fibonacci.log)
+
---
+++ Python OOP/Lecture 18/demo.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+    if n < 2:
+        return n
+
+    else:
+
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+for x in range(10):
+    print(fibonacci(x))
