1e7407d61b05b7d069afa405bb46f6a09791a1f8
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-10-04 20:06:10 +0300

    Added Constructor Test

--- Python OOP/Lecture 22/project/test/test.py
+++ Python OOP/Lecture 22/project/test/test.py
@@ -1,1 +1,18 @@
 from project.pet_shop import PetShop
+from unittest import TestCase, main
+
+
+class PetShopTest(TestCase):
+    def setUp(self) -> None:
+        self.pet_shop = PetShop("Dogs")
+
+    def test_constructor(self):
+        self.assertEqual(self.pet_shop.name, "Dogs")
+        self.assertEqual(self.pet_shop.food, {})
+        self.assertEqual(self.pet_shop.pets, [])
+
+
+
+if __name__ == '__main__':
+    main()
+
