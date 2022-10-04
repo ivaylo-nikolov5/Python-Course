603b63163642c199bf78cd18dc0c949cde3c43d1
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-10-04 12:59:05 +0300

    Implemented Reserve Table method

--- Python OOP/Lecture 22/exam_skeleton/project/bakery.py
+++ Python OOP/Lecture 22/exam_skeleton/project/bakery.py
@@ -2,10 +2,15 @@ from project.baked_food.cake import Cake
 from project.baked_food.bread import Bread
 from project.drink.tea import Tea
 from project.drink.water import Water
+from project.table.inside_table import InsideTable
+from project.table.outside_table import OutsideTable
+from project.core.validator import Validator
+
 
 class Bakery:
     FOOD_TYPES = [Bread, Cake]
     DRINK_TYPES = [Tea, Water]
+    TABLE_TYPES = [InsideTable, OutsideTable]
 
     def __init__(self, name):
         self.name = name
@@ -19,18 +24,28 @@ class Bakery:
             self.raise_already_in_the_menu(food_type, name)
         elif food_type in Bakery.FOOD_TYPES:
             self.food_menu.append(Bakery.FOOD_TYPES[food_type](name, price))
+            return f"Added {name} ({food_type}) to the food menu"
 
     def add_drink(self, drink_type, name: str, portion: int, brand:str):
         if self.check_name_existence(self.drinks_menu, name):
             self.raise_already_in_the_menu(drink_type, name)
         elif drink_type in self.DRINK_TYPES:
             self.drinks_menu.append(Bakery.DRINK_TYPES[drink_type](name, portion, brand))
+            return f"Added {name} ({brand}) to the drink menu"
 
-    def add_table(self, table_type: str, table_number: int, capacity: int):
-        pass
+    def add_table(self, table_type, table_number: int, capacity: int):
+        Validator.raise_if_table_already_exists(self.tables_repository, table_number)
+        if table_type in Bakery.TABLE_TYPES:
+            self.tables_repository.append(Bakery.TABLE_TYPES[table_type](table_number, capacity))
+            return f"Added table number {table_number} in the bakery"
 
     def reserve_table(self, number_of_people: int):
-        pass
+        available_table = self.find_not_reserved_table(number_of_people)
+        if not available_table:
+            return f"No available table for {number_of_people} people"
+        table_number, table = available_table
+        self.tables_repository[table].reserved = True
+        return f"Table {table_number} has been reserved for {number_of_people} people"
 
     def order_food(self, table_number: int, *args):
         pass
@@ -57,3 +72,9 @@ class Bakery:
     @staticmethod
     def raise_already_in_the_menu(product_type, name):
         raise ValueError(f"{product_type} {name} is already in the menu!")
+
+    def find_not_reserved_table(self, people_count):
+        for table in self.tables_repository:
+            if not table.reserved and table.capacity >= people_count:
+                return [table.table_number, table]
+        return False
--- Python OOP/Lecture 22/exam_skeleton/project/core/validator.py
+++ Python OOP/Lecture 22/exam_skeleton/project/core/validator.py
@@ -8,3 +8,10 @@ class Validator:
     def raise_if_number_is_less_or_equal_to_zero(number, string):
         if number <= 0:
             raise ValueError(string)
+
+    @staticmethod
+    def raise_if_table_already_exists(tables, table_number):
+        for table in tables:
+            if table.table_number == table_number:
+                raise Exception(f"Table {table_number} is already in the bakery!")
+
