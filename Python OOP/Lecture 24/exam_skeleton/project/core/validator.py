c79a58e1b342bfc25370171e8a8ec9326e5003bd
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-10-06 15:14:16 +0300

    Implemented the Base Fish class

--- Python OOP/Lecture 24/exam_skeleton/project/core/validator.py
+++ Python OOP/Lecture 24/exam_skeleton/project/core/validator.py
@@ -1,2 +1,10 @@
 class Validator:
-    pass
+    @staticmethod
+    def raise_if_value_is_empty_string(value, text):
+        if not value.srtip():
+            raise ValueError(text)
+
+    @staticmethod
+    def raise_if_value_is_zero_or_negative(value, text):
+        if value <= 0:
+            raise ValueError(text)
