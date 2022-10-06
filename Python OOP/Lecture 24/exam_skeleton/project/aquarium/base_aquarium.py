ad76c893c9030a70d3d672cc68bea45a070f7e5f
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-10-06 16:10:50 +0300

    Implemented Base Aquarium class

--- Python OOP/Lecture 24/exam_skeleton/project/aquarium/base_aquarium.py
+++ Python OOP/Lecture 24/exam_skeleton/project/aquarium/base_aquarium.py
@@ -1,9 +1,13 @@
 from abc import ABC, abstractmethod
 from project.core.validator import Validator
-from project.core.calculations import Calculations
+from project.fish.freshwater_fish import FreshwaterFish
+from project.fish.saltwater_fish import SaltwaterFish
 
 
 class BaseAquarium(ABC):
+    POSSIBLE_FISH_TYPES = [FreshwaterFish, SaltwaterFish]
+
+    @abstractmethod
     def __init__(self, name: str, capacity: int):
         self.name = name
         self.capacity = capacity
@@ -25,16 +29,30 @@ class BaseAquarium(ABC):
         return comfort
 
     def add_fish(self, fish):
-        pass
+        if len(fish) == self.capacity:
+            return "Not enough capacity."
+        elif fish.__class__.__name__ in BaseAquarium.POSSIBLE_FISH_TYPES:
+            self.fish.append(fish)
+            return f"Successfully added {fish.__class__.__name__} to {self.name}."
 
     def remove_fish(self, fish):
-        pass
+        self.fish.remove(fish)
 
     def add_decoration(self, decoration):
-        pass
+        self.decorations.append(decoration)
 
-    def feed(self, ):
-        pass
+    def feed(self):
+        for fish in self.fish:
+            fish.feed()
 
-    def __str__(self, ):
-        pass
+    def __str__(self):
+        result = f"{self.name}:\n"
+        if not self.fish:
+            result += "Fish: none\n"
+        else:
+            result += f"Fish: {' '.join(fish.name for fish in self.fish)}\n" \
+
+        result += f"Decorations: {len(self.decorations)}\n" \
+                  f"Comfort: {self.calculate_comfort()}"
+
+        return result
