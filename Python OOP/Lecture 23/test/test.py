006e6df45be786be0e9f0c17272d4e771a1fb465
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-10-06 09:33:10 +0300

    Added the tests for the: add_member, remove_member and __gt__ methods

--- Python OOP/Lecture 23/test/test.py
+++ Python OOP/Lecture 23/test/test.py
@@ -1,1 +1,65 @@
+from unittest import TestCase, main
 from project.team import Team
+
+
+class TeamTest(TestCase):
+    def setUp(self) -> None:
+        self.team = Team("Bulgarians")
+
+    def test_constructor(self):
+        self.assertEqual("Bulgarians", self.team.name)
+        self.assertEqual({}, self.team.members)
+
+    def test_name_attribute_with_numbers_and_other_symbols_input(self):
+        with self.assertRaises(ValueError) as ve:
+            team = Team("124")
+        expected = "Team Name can contain only letters!"
+        self.assertEqual(expected, str(ve.exception))
+
+        with self.assertRaises(ValueError) as ve:
+            team = Team("./,!@#%$^")
+        self.assertEqual(expected, str(ve.exception))
+
+    def test_add_member_method(self):
+        result = self.team.add_member(Ivaylo=17, Emin=16)
+        expected = {'Ivaylo': 17, 'Emin': 16}
+        self.assertEqual(expected, self.team.members)
+
+        expected = "Successfully added: Ivaylo, Emin"
+        self.assertEqual(expected, result)
+
+    def test_remove_name_method_with_existing_name(self):
+        self.team.add_member(Ivaylo=17, Emin=16)
+        result = self.team.remove_member("Ivaylo")
+        expected = {'Emin': 16}
+        self.assertEqual(expected, self.team.members)
+
+        expected = "Member Ivaylo removed"
+        self.assertEqual(expected, result)
+
+    def test_remove_name_method_with_non_existing_name(self):
+        self.team.add_member(Ivaylo=17, Emin=16)
+        result = self.team.remove_member("Name")
+        expected = "Member with name Name does not exist"
+        self.assertEqual(expected, result)
+
+    def test_greater_than_method_with_true_output(self):
+        other_team = Team("Barbarians")
+        self.team.add_member(Ivaylo=17, Emin=16)
+        other_team.add_member(barbarian=15)
+        result = self.team > other_team
+        self.assertTrue(result)
+
+    def test_greater_than_method_with_false_output(self):
+        other_team = Team("Barbarians")
+        self.team.add_member(Ivaylo=17, Emin=16)
+        other_team.add_member(barbarian=15, barbarian2=19)
+        result = self.team > other_team
+        self.assertFalse(result)
+
+        other_team.add_member(barbarian3=21)
+        result = self.team > other_team
+        self.assertFalse(result)
+
+if __name__ == '__main__':
+    main()
