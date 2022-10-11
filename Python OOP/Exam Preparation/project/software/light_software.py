309a66017d016b636a65813bc58ee3349876a07a
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-10-11 13:04:24 +0300

    Implemented Software types

---
+++ Python OOP/Exam Preparation/project/software/light_software.py
@@ -0,0 +1,8 @@
+from project.software.software import Software
+
+
+class LightSoftware(Software):
+    def __init__(self, name: str, capacity_consumption: int, memory_consumption: int):
+        super().__init__(name, "Light", capacity_consumption, memory_consumption)
+        self.capacity_consumption += self.capacity_consumption // 2
+        self.memory_consumption -= self.memory_consumption // 2
