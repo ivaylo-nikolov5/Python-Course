9bf2788ab8e83f5b74f4905756f71d19e034d5d8
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-10-18 14:34:51 +0300

    Took the Exam brudda les go but not with 300/300 but still took it lessssss goooo

--- Python OOP/Exam Retake/demo.py
+++ Python OOP/Exam Retake/demo.py
@@ -1,24 +1,6 @@
-from project_.movie_app import MovieApp
-from project_.movie_specification.fantasy import Fantasy
-from project_.movie_specification.action import Action
+from project.plantation import Plantation
 
-movie_app = MovieApp()
-print(movie_app.register_user('Martin', 24))
-user = movie_app.users_collection[0]
-movie = Action('Die Hard', 1988, user, 18)
-print(movie_app.upload_movie('Martin', movie))
-print(movie_app.movies_collection[0].title)
-print(movie_app.register_user('Alexandra', 25))
-user2 = movie_app.users_collection[1]
-movie2 = Action('Free Guy', 2021, user2, 16)
-print(movie_app.upload_movie('Alexandra', movie2))
-print(movie_app.edit_movie('Alexandra', movie2, title="Free Guy 2"))
-print(movie_app.like_movie('Martin', movie2))
-print(movie_app.like_movie('Alexandra', movie))
-print(movie_app.dislike_movie('Martin', movie2))
-print(movie_app.like_movie('Martin', movie2))
-print(movie_app.delete_movie('Alexandra', movie2))
-movie2 = Fantasy('The Lord of the Rings', 2003, user2, 14)
-print(movie_app.upload_movie('Alexandra', movie2))
-print(movie_app.display_movies())
-print(movie_app)
+plantation = Plantation(10)
+plantation.workers = ["Ivan", "Andrey"]
+plantation.plants = {"Ivan": ["rice", "smth"], "Andrey": ["sugar cane", "smth"]}
+print(plantation)
--- Python OOP/Exam Retake/project/test/test_plantation.py
+++ Python OOP/Exam Retake/project/test/test_plantation.py
@@ -1,1 +1,82 @@
 from project.plantation import Plantation
+from unittest import TestCase, main
+
+
+class PlantationTest(TestCase):
+    def setUp(self) -> None:
+        self.plantation = Plantation(10)
+
+    def test_constructor_properly_initialize(self):
+        self.assertEqual(10, self.plantation.size)
+        self.assertEqual({}, self.plantation.plants)
+        self.assertEqual([], self.plantation.workers)
+
+    def test_size_with_negative_number(self):
+        with self.assertRaises(ValueError) as ex:
+            self.plantation.size = -20
+        expected = "Size must be positive number!"
+        self.assertEqual(expected, str(ex.exception))
+
+    def test_hire_worker_raises(self):
+        self.plantation.workers = ["Ivan"]
+        with self.assertRaises(ValueError) as ex:
+            self.plantation.hire_worker("Ivan")
+        expected = "Worker already hired!"
+        self.assertEqual(expected, str(ex.exception))
+
+    def test_hire_worker_with_valid_data(self):
+        result = self.plantation.hire_worker("Ivan")
+        self.assertEqual(["Ivan"], self.plantation.workers)
+        expected = "Ivan successfully hired."
+        self.assertEqual(expected, result)
+
+    def test_dunder_len_returns_the_length_of_the_plants(self):
+        self.plantation.plants = {"a": "ab", "b": "abc", "c": "abcd"}
+        self.assertEqual(9, len(self.plantation))
+
+    def test_planting_with_non_existing_worker(self):
+        with self.assertRaises(ValueError) as ex:
+            self.plantation.planting("Ivan", "sugar cane")
+        expected = "Worker with name Ivan is not hired!"
+        self.assertEqual(expected, str(ex.exception))
+
+    def test_planting_with_more_quantity(self):
+        self.plantation.plants = {"Ivan": ["idk what is that", "a", "b", "a", "b", "a", "b", "a", "b", "a", "b"]}
+        self.plantation.workers = ["Ivan"]
+        with self.assertRaises(ValueError) as ex:
+            self.plantation.planting("Ivan", "plant with long name")
+        expected = "The plantation is full!"
+        self.assertEqual(expected, str(ex.exception))
+
+    def test_planting_with_worker_who_already_has_been_planting(self):
+        self.plantation.plants = {"Ivan": ["wheat"]}
+        self.plantation.workers = ["Ivan"]
+        result = self.plantation.planting("Ivan", "rice")
+        self.assertEqual({"Ivan": ["wheat", "rice"]}, self.plantation.plants)
+        self.assertEqual("Ivan planted rice.", result)
+
+    def test_planting_with_new_worker(self):
+        self.plantation.workers = ["Ivan"]
+        result = self.plantation.planting("Ivan", "rice")
+        self.assertEqual({"Ivan": ["rice"]}, self.plantation.plants)
+        self.assertEqual("Ivan planted it's first rice.", result)
+
+    def test_dunder_str(self):
+        self.plantation.workers = ["Ivan", "Andrey"]
+        self.plantation.plants = {"Ivan": ["rice", "smth"], "Andrey": ["sugar cane", "smth"]}
+        result = str(self.plantation)
+        expected = "Plantation size: 10\n" \
+                   "Ivan, Andrey\n" \
+                   "Ivan planted: rice, smth\n" \
+                   "Andrey planted: sugar cane, smth"
+        self.assertEqual(expected, result)
+
+    def test_dunder_repr(self):
+        self.plantation.workers = ["Ivan", "Andrey"]
+        result = repr(self.plantation)
+        expected = "Size: 10\nWorkers: Ivan, Andrey"
+        self.assertEqual(expected, result)
+
+
+if __name__ == '__main__':
+    main()
