9a753d85c01bb42eb136c37b412f4731c81de984
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-10-21 10:38:47 +0300

    Implemented pop method

---
+++ Data Structures and Algorithms/Stacks and Queues/stack/stack.py
@@ -0,0 +1,15 @@
+from stack.exceptions import EmptyStack
+
+
+class Stack:
+    def __init__(self):
+        self.__stack = []
+
+    def get_stack(self):
+        return self.__stack
+
+    def pop(self):
+        if not self.__stack:
+            raise EmptyStack("You cannot pop an item from an empty stack!")
+        item = self.__stack.pop()
+        return item
