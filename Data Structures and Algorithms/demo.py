919a5db4449f2dd9751a24cb72b3e94ca27937fc
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-10-24 15:38:47 +0300

    implemented traverse method

--- Data Structures and Algorithms/demo.py
+++ Data Structures and Algorithms/demo.py
@@ -1,17 +1,43 @@
-from binary_search_tree import BinarySearchTree
-
-tree = BinarySearchTree()
-tree.insert(100)
-tree.insert(50)
-tree.insert(25)
-tree.insert(75)
-tree.insert(150)
-tree.insert(125)
-tree.insert(175)
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left_value = None
+        self.right_value = None
 
 
-print(tree.get_min_value())
-print(tree.get_max_value())
-tree.traverse()
-tree.remove(100)
-tree.traverse()
+class BinarySearchTree:
+    def __init__(self):
+        self.__root = None
+        self.vals = []
+
+    def insert(self, value):
+        if not self.__root:
+            self.__root = Node(value)
+            return print("Inserted value as root...")
+        return self.__insert_node(self.__root, value)
+
+    def __insert_node(self, node, value):
+        if value < node.value:
+            if node.left_value:
+                return self.__insert_node(node.left_value, value)
+            node.left_value = Node(value)
+        else:
+            if node.right_value:
+                return self.__insert_node(node.right_value, value)
+            node.right_value = Node(value)
+            return
+
+    def traverse(self):
+        if not self.__root:
+            return "You cannot traverse an empty binary searching tree!"
+        self.__traverse_in_order(self.__root)
+
+    def __traverse_in_order(self, node):
+        if node.left_value:
+            self.__traverse_in_order(node.left_value)
+
+        print(node.value)
+
+        if node.right_value:
+            self.__traverse_in_order(node.right_value)
+
---
+++ Data Structures and Algorithms/demo2.py
@@ -0,0 +1,12 @@
+from demo import BinarySearchTree
+
+bsr = BinarySearchTree()
+bsr.insert(100)
+bsr.insert(50)
+bsr.insert(120)
+bsr.insert(20)
+bsr.insert(45)
+bsr.insert(260)
+bsr.insert(76)
+
+bsr.traverse()
