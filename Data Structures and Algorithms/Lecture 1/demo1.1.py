80360c4deb3f118e460087a75e21cd46fc7f4140
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-10-27 16:23:03 +0300

    Revised the Recursive Drawing problem

--- Data Structures and Algorithms/Lecture 1/demo1.1.py
+++
@@ -1,8 +0,0 @@
-def recursive_factorial(n):
-    if n == 0:
-        return 1
-    return n * recursive_factorial(n - 1)
-
-
-number = int(input())
-print(recursive_factorial(number))
--- Data Structures and Algorithms/Lecture 1/recursive_drawing.py
+++ Data Structures and Algorithms/Lecture 1/recursive_drawing.py
@@ -1,10 +1,11 @@
-def draw_recursion(n):
+def recursive_drawing(n):
     if n == 0:
         return
 
     print("*" * n)
-    draw_recursion(n - 1)
+    recursive_drawing(n - 1)
     print("#" * n)
 
-a = int(input())
-draw_recursion(a)
+
+lines = int(input())
+recursive_drawing(lines)
