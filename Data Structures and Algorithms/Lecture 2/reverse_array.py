5403417e856562a500c65894ed46df908d19ae28
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-10-28 15:13:03 +0300

    Added a solution for the Reverse Array problem

---
+++ Data Structures and Algorithms/Lecture 2/reverse_array.py
@@ -0,0 +1,9 @@
+def reverse_array(arr, idx):
+    if idx == len(arr) - 1:
+        print(*arr, sep=" ")
+        return
+    arr.append(arr.pop(-idx - 2))
+    return reverse_array(arr, idx + 1)
+
+array = [x for x in input().split()]
+reverse_array(array, 0)
