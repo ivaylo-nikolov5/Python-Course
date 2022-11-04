4ea655adf1c5555735e804690985834ebce3af4a
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-11-04 23:06:00 +0200

    Revised the Binary Search problem

--- Data Structures and Algorithms/Lecture 3/demo.py
+++ Data Structures and Algorithms/Lecture 3/demo.py
@@ -1,6 +1,23 @@
+def binary_search(nums, target):
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+        mid_idx = (right + left) // 2
+        mid_el = nums[mid_idx]
+
+        if mid_el == target:
+            return mid_idx
+
+        if mid_el < target:
+            left += 1
+        else:
+            right -= 1
+
+    return -1
 
 
 numbers = [int(x) for x in input().split()]
 target = int(input())
 
-print(binary_search(numbers, target, 0, len(numbers)))
+print(binary_search(numbers, target))
