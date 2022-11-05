9e725e90d86252e42144312c7a94b0e209e9e723
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-11-05 09:23:58 +0200

    Revised the Selection Sort problem

--- Data Structures and Algorithms/Lecture 3/demo.py
+++ Data Structures and Algorithms/Lecture 3/demo.py
@@ -1,23 +1,40 @@
-def binary_search(nums, target):
-    left = 0
-    right = len(nums) - 1
+# def binary_search(nums, target):
+#     left = 0
+#     right = len(nums) - 1
+#
+#     while left <= right:
+#         mid_idx = (right + left) // 2
+#         mid_el = nums[mid_idx]
+#
+#         if mid_el == target:
+#             return mid_idx
+#
+#         if mid_el < target:
+#             left += 1
+#         else:
+#             right -= 1
+#
+#     return -1
+#
+#
+# numbers = [int(x) for x in input().split()]
+# target = int(input())
+#
+# print(binary_search(numbers, target))
 
-    while left <= right:
-        mid_idx = (right + left) // 2
-        mid_el = nums[mid_idx]
 
-        if mid_el == target:
-            return mid_idx
+##################################################################################################
 
-        if mid_el < target:
-            left += 1
-        else:
-            right -= 1
 
-    return -1
+def selection_sort(nums):
+    for i in range(len(nums)):
+        for j in range(i, len(nums)):
+            if nums[j] < nums[i]:
+                nums[i], nums[j] = nums[j], nums[i]
+
+    return nums
 
 
 numbers = [int(x) for x in input().split()]
-target = int(input())
 
-print(binary_search(numbers, target))
+print(*selection_sort(numbers), sep=" ")
