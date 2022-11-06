d2684fbbb02389f63534176b2e2b973b74848a19
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-11-06 16:48:09 +0200

    Implemented BFS

---
+++ Data Structures and Algorithms/Lecture 4/bfs.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+
+def bfs(node, graph, queue, visited):
+    if node in visited:
+        return
+
+    queue.appendleft(node)
+    visited.add(node)
+
+    while queue:
+        n = queue.popleft()
+        print(n)
+        for child in graph[n]:
+            if child not in visited:
+                print(child)
+                visited.add(child)
+
+    return visited
+
+graph = {
+    7: [19, 21, 14],
+    19: [1, 12, 31, 21],
+    1: [7],
+    12: [],
+    31: [21],
+    21: [14],
+    14: [6, 23],
+    23: [21],
+    6: []
+}
+
+queue = deque()
+visited = set()
+
+for node in graph:
+    bfs(node, graph, queue, visited)
