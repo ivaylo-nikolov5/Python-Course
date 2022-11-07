a2d38e55c06db4abb2645965cd5a472e596c85f1
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-11-07 18:31:08 +0200

    Added a solution for the Topological Sorting problem

---
+++ Data Structures and Algorithms/Lecture 4/topological_sorting.py
@@ -0,0 +1,55 @@
+def find_nodes_with_edges(graph):
+    result = {}
+    for node, children in graph.items():
+        if node not in result:
+            result[node] = 0
+        for child in children:
+            if child not in result:
+                result[child] = 1
+            else:
+                result[child] += 1
+
+    return result
+
+
+def find_nodes_without_dependencies(dependencies_by_node):
+    for node, dependencies in dependencies_by_node.items():
+        if dependencies == 0:
+            return node
+
+    return None
+
+
+lines = int(input())
+
+graph = {}
+
+for _ in range(lines):
+    line = input().split("->")
+    node = line[0].strip()
+    children = [x for x in line[1].strip().split(", ")] if line[1] else []
+    graph[node] = children
+
+nodes_with_edges = find_nodes_with_edges(graph)
+
+
+sorted_nodes = []
+
+has_cycles = False
+
+while nodes_with_edges:
+    node_to_remove = find_nodes_without_dependencies(nodes_with_edges)
+    if node_to_remove is None:
+        has_cycles = True
+        break
+
+    nodes_with_edges.pop(node_to_remove)
+    sorted_nodes.append(node_to_remove)
+    for child in graph[node_to_remove]:
+        nodes_with_edges[child] -= 1
+
+
+if has_cycles:
+    print("Invalid topological sorting")
+else:
+    print(f"Topological sorting: {', '.join(sorted_nodes)}")
