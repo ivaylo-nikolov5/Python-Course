75444b028e26766048cfc2ec854b47f8eaed858b
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-11-08 12:23:43 +0200

    Revised the Topological Sorting problem

--- Data Structures and Algorithms/Lecture 4/demo.py
+++ Data Structures and Algorithms/Lecture 4/demo.py
@@ -1,28 +1,52 @@
-def dfs(node, graph, visited, components):
-    if visited[node]:
-        return
+def find_nodes_with_edges(graph):
+    result = {}
+    for node, children in graph.items():
+        if node not in result:
+            result[node] = 0
+        for child in children:
+            if child in result:
+                result[child] += 1
+            else:
+                result[child] = 1
 
-    visited[node] = True
-    for child in graph[node]:
-        dfs(child, graph, visited, components)
+    return result
 
-    components.append(node)
 
-    return components
+def find_nodes_dependencies(nodes_with_dependencies):
+    for node, dependencies in nodes_with_dependencies.items():
+        if dependencies == 0:
+            return node
+
+    return None
 
 
 lines = int(input())
 
-graph = []
+graph = {}
 
 for _ in range(lines):
-    line = input()
-    children = [] if not line else [int(x) for x in line.split()]
-    graph.append(children)
+    line = input().split("->")
+    node = line[0].strip()
+    children = [] if not line[1].strip() else [x for x in line[1].strip().split(", ")]
+    graph[node] = children
 
-visited = [False] * lines
+nodes_with_edges = find_nodes_with_edges(graph)
+sorted_nodes = []
+has_cycles = False
 
-for node in range(len(graph)):
-    result = dfs(node, graph, visited, [])
-    if result:
-        print(f"Connected component: {' '.join(str(x) for x in result)}")
+while nodes_with_edges:
+    node_to_remove = find_nodes_dependencies(nodes_with_edges)
+    if node_to_remove is None:
+        has_cycles = True
+        break
+
+    sorted_nodes.append(node_to_remove)
+    nodes_with_edges.pop(node_to_remove)
+    for child in graph[node_to_remove]:
+        nodes_with_edges[child] -= 1
+
+
+if has_cycles:
+    print("Invalid topological sorting")
+else:
+    print(f"Topological sorting: {', '.join(str(x) for x in sorted_nodes)}")
