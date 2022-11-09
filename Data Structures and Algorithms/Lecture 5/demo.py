9bf3423be5eb94d12c61a309132e1e47157d7f67
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-11-09 20:47:36 +0200

    Added a solution for the Shortest Path in Unweighted Graph problem

--- Data Structures and Algorithms/Lecture 5/demo.py
+++ Data Structures and Algorithms/Lecture 5/demo.py
@@ -1,52 +1,17 @@
-def dfs(node, destination, graph, visited):
-    if node in visited:
-        return
+buildings = int(input())
+streets = int(input())
 
-    visited.add(node)
-
-    if node == destination:
-        return
-
-    for child in graph[node]:
-        dfs(child, destination, graph, visited)
+print("Important streets:")
+graph = [None] * buildings
 
 
-def path_exists(source, destination, graph):
-    visited = set()
+for _ in range(streets):
+    line = input().split(" - ")
+    building = int(line[0])
+    connected_building = int(line[1])
+    if not graph[building]:
+        graph[building] = []
 
-    dfs(source, destination, graph, visited)
-
-    return destination in visited
+    graph[building].append()
 
 
-lines = int(input())
-
-graph = {}
-edges = []
-edges_to_remove = []
-
-for _ in range(lines):
-    line = input().split(" -> ")
-    node = line[0]
-    children = [x for x in line[1].split()]
-    graph[node] = children
-    for child in children:
-        edges.append((node, child))
-
-for source, destination in list(sorted(edges, key=lambda x: (x[0], x[1]))):
-    if source not in graph[destination] or destination not in graph[source]:
-        continue
-
-    graph[source].remove(destination)
-    graph[destination].remove(source)
-
-    if path_exists(source, destination, graph):
-        edges_to_remove.append((source, destination))
-    else:
-        graph[source].append(destination)
-        graph[destination].append(source)
-
-print(f"Edges to remove: {len(edges_to_remove)}")
-
-for x, y in edges_to_remove:
-    print(f"{x} - {y}")
