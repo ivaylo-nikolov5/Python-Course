ab84954309c09b67d78b7778536d883696cfbe4e
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-11-12 13:10:50 +0200

    Revised the Bellman-Ford problem

---
+++ Data Structures and Algorithms/Lecture 6/demo.py
@@ -0,0 +1,64 @@
+from collections import deque
+
+
+class Edge:
+    def __init__(self, source, destination, weight):
+        self.source = source
+        self.destination = destination
+        self.weight = weight
+
+
+nodes = int(input())
+edges = int(input())
+
+graph = []
+
+for _ in range(edges):
+    source, destination, weight = [int(x) for x in input().split()]
+    graph.append(Edge(source, destination, weight))
+
+start = int(input())
+end = int(input())
+
+distance = [float("inf")] * (nodes + 1)
+distance[start] = 0
+
+parent = [None] * (nodes + 1)
+
+for _ in range(nodes - 1):
+    updated = False
+
+    for edge in graph:
+        if distance[edge.source] == float("inf"):
+            continue
+        new_distance = distance[edge.source] + edge.weight
+        if new_distance < distance[edge.destination]:
+            distance[edge.destination] = new_distance
+            parent[edge.destination] = edge.source
+            updated = True
+
+    if not updated:
+        break
+
+
+updated = False
+for edge in graph:
+    new_distance = distance[edge.source] + edge.weight
+    if new_distance < distance[edge.destination]:
+        distance[edge.destination] = new_distance
+        parent[edge.destination] = edge.source
+        updated = True
+
+if updated:
+    print("Negative Cycle Detected")
+else:
+    path = deque()
+    node = end
+
+    while node:
+        path.appendleft(node)
+        node = parent[node]
+
+    print(*path, sep=" ")
+    print(distance[end])
+
