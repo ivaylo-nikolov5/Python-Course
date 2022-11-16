f062042e9eecf30dd3f31facc4dfb56c659d5eed
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-11-16 15:45:56 +0200

    Revised the Cheap Town Tour  problem

--- Data Structures and Algorithms/Lecture 7/demo.py
+++ Data Structures and Algorithms/Lecture 7/demo.py
@@ -1,59 +1,30 @@
-from collections import deque
-from queue import PriorityQueue
+def find_root(node, parent):
+    while node != parent[node]:
+        node = parent[node]
 
-
-class Edge:
-    def __init__(self, source, destination, weight):
-        self.source = source
-        self.destination = destination
-        self.weight = weight
+    return node
 
 
 nodes = int(input())
 edges = int(input())
 
 graph = []
-[graph.append([]) for _ in range(nodes)]
-parent = [None] * nodes
-distance = [float("-inf")] * nodes
+max_num = float("-inf")
 
 for _ in range(edges):
-    source, destination, weight = [int(x) for x in input().split()]
-    edge = Edge(source, destination, weight)
+    first, second, weight = [int(num) for num in input().split(" - ")]
+    graph.append((first, second, weight))
+    max_num = max(first, second, max_num)
 
-    graph[source].append(edge)
-    graph[destination].append(edge)
+parent = [num for num in range(max_num + 1)]
+total_cost = 0
 
-start_node = int(input())
-end_node = int(input())
+for first, second, weight in sorted(graph, key=lambda x: x[2]):
+    first_root_node = find_root(first, parent)
+    second_root_node = find_root(second, parent)
 
-distance[start_node] = 100
+    if first_root_node != second_root_node:
+        parent[first_root_node] = second_root_node
+        total_cost += weight
 
-pq = PriorityQueue()
-pq.put((-100, start_node))
-
-while not pq.empty():
-    max_distance, node = pq.get()
-
-    if node == end_node:
-        break
-
-    for edge in graph[node]:
-        child = edge.destination if edge.source == node else edge.source
-        new_distance = -max_distance * edge.weight / 100
-        if new_distance > distance[child]:
-            distance[child] = new_distance
-            parent[child] = node
-            pq.put((-new_distance, child))
-
-print(f"Most reliable path reliability: {distance[end_node]:.2f}%")
-
-path = deque()
-node = end_node
-
-while node:
-    path.appendleft(node)
-    node = parent[node]
-path.appendleft(start_node)
-print(*path, sep=" -> ")
-
+print(f"Total cost: {total_cost}")
