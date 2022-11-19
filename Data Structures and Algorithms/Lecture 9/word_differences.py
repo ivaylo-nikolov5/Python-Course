51116a34f7f9bc316c573102472b7f69e36405ef
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-11-19 19:58:10 +0200

    Added a second solution for the Word Differences problem

--- Data Structures and Algorithms/Lecture 9/word_differences.py
+++ Data Structures and Algorithms/Lecture 9/word_differences.py
@@ -22,3 +22,43 @@ for row in range(1, rows):
 result = dp[rows - 1][cols - 1]
 
 print(f"Deletions and Insertions: {result}")
+
+
+# ---------------------------------------------------------------------
+
+# That's a solution with swapping letters and finding the quickest way to equalize the words:
+
+first = [el for el in input()]
+second = [el for el in input()]
+
+insertions_and_deletions = 0
+
+for first_idx in range(len(first)):
+    if second[first_idx] == first[first_idx]:
+        continue
+
+    for sec_idx in range(len(second)):
+        if second[sec_idx] == first[first_idx]:
+            second[sec_idx], second[first_idx] = second[first_idx], second[sec_idx]
+            insertions_and_deletions += 2
+
+
+while len(first) != len(second):
+    if len(first) < len(second):
+        insertions_and_deletions += len(second) - len(first)
+        first.extend(second[len(first):])
+    else:
+        insertions_and_deletions += len(first) - len(second)
+        second.extend(first[len(second):])
+
+
+for idx in range(len(first)):
+    if first == second:
+        break
+    elif first[idx] == second[idx]:
+        continue
+
+    first[idx] = second[idx]
+    insertions_and_deletions += 2
+
+print(f"Deletions and Insertions: " + str(insertions_and_deletions))
