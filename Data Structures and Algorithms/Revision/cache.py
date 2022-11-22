c772d3e07607a7656350cae5e6238a50b5c88361
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-11-22 18:55:35 +0200

    Added an iterative solution for the Paths in Labyrinth problem

--- Data Structures and Algorithms/Revision/cache.py
+++ Data Structures and Algorithms/Revision/cache.py
@@ -99,6 +99,48 @@ generate_vectors(number, 0, [])
 # -----------------------------------------------------------------------------------------------
 
 
+def is_inside(row, col, rows, cols):
+    if row < 0 or col < 0 or row >= rows or col >= cols:
+        return False
+    return True
+
+
+def find_paths(row, col, rows, cols, matrix, path, direction):
+    if not is_inside(row, col, rows, cols):
+        return
+    elif matrix[row][col] == "*" or matrix[row][col] == "v":
+        return
+    elif matrix[row][col] == "e":
+        path.append(direction)
+        print(*path, sep="")
+        path.pop()
+        return
+
+    path.append(direction)
+    matrix[row][col] = "v"
+
+    find_paths(row, col + 1, rows, cols, matrix, path, "R")
+    find_paths(row + 1, col, rows, cols, matrix, path, "D")
+    find_paths(row, col - 1, rows, cols, matrix, path, "L")
+    find_paths(row - 1, col, rows, cols, matrix, path, "U")
+
+    matrix[row][col] = "-"
+    path.pop()
+
+
+rows = int(input())
+cols = int(input())
+
+matrix = []
+for _ in range(rows):
+    row = input()
+    matrix.append([x for x in row])
+
+find_paths(0, 0, rows, cols, matrix, [], "")
+
+
+# ----------------------------------------------------------------------------------------------------
+
 
 
 
