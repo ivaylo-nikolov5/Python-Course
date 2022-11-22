eae71cc2d818244b9083b92944971e590b3fdd15
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-11-22 17:13:23 +0200

    Added a iterative solution for the Recursive Factorial problem

--- Data Structures and Algorithms/Revision/cache.py
+++ Data Structures and Algorithms/Revision/cache.py
@@ -36,6 +36,15 @@ number = int(input())
 print(factorial(number))
 
 
+factorial = 1
+
+while number > 0:
+    factorial *= number
+    number -= 1
+
+print(factorial)
+
+
 # -----------------------------------------------------------------------------
 
 
--- Data Structures and Algorithms/Revision/recursive_factorial.py
+++ Data Structures and Algorithms/Revision/recursive_factorial.py
@@ -1,10 +1,21 @@
-def factorial(n):
-    if n <= 1:
-        return 1
-
-    return n * factorial(n - 1)
+# def factorial(n):
+#     if n <= 1:
+#         return 1
+#
+#     return n * factorial(n - 1)
+#
+#
+# number = int(input())
+#
+# print(factorial(number))
 
 
 number = int(input())
 
-print(factorial(number))
+factorial = 1
+
+while number > 0:
+    factorial *= number
+    number -= 1
+
+print(factorial)
