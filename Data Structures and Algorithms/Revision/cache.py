75541c676e0397d606fa1e3dfd82a31c48c54891
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-11-22 15:34:14 +0200

    Revised the Recursive Array Sum problem and added it to the cache file

---
+++ Data Structures and Algorithms/Revision/cache.py
@@ -0,0 +1,26 @@
+def recursive_array_sum(array, idx):
+    result = 0
+
+    if idx == len(array):
+        return result
+
+    return array[idx] + recursive_array_sum(array, idx + 1)
+
+
+array = [int(num) for num in input().split()]
+
+array_sum = recursive_array_sum(array, 0)
+print(array_sum)
+
+
+array_sum = 0
+
+while array:
+    array_sum += array.pop()
+
+print(array_sum)
+
+
+# ---------------------------------------------------------------------------------------------------------
+
+
--- Data Structures and Algorithms/Revision/recursive_array_sum.py
+++ Data Structures and Algorithms/Revision/recursive_array_sum.py
@@ -1,19 +1,17 @@
-# def recursive_array_sum(array, idx):
-#     result = 0
-#
-#     if idx == len(array):
-#         return result
-#
-#     return array[idx] + recursive_array_sum(array, idx + 1)
-#
-#
-# array = [int(num) for num in input().split()]
-#
-# array_sum = recursive_array_sum(array, 0)
-# print(array_sum)
+def recursive_array_sum(array, idx):
+    result = 0
+
+    if idx == len(array):
+        return result
+
+    return array[idx] + recursive_array_sum(array, idx + 1)
 
 
 array = [int(num) for num in input().split()]
+
+array_sum = recursive_array_sum(array, 0)
+print(array_sum)
+
 
 array_sum = 0
 
