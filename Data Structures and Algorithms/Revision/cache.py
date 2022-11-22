85682d0c8e101ae74732ab780e25c87393e75a85
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-11-22 17:48:21 +0200

    Added an iterative solution for the Recursive Drawing problem

--- Data Structures and Algorithms/Revision/cache.py
+++ Data Structures and Algorithms/Revision/cache.py
@@ -63,6 +63,19 @@ number = int(input())
 
 recursive_drawing(number)
 
+number = int(input())
+
+temp = number
+
+while temp > 0:
+    print("*" * temp)
+    temp -= 1
+
+temp += 1
+
+while temp <= number:
+    print("#" * temp)
+    temp += 1
 
 # ---------------------------------------------------------------------------
 
--- Data Structures and Algorithms/Revision/recursive_drawing.py
+++ Data Structures and Algorithms/Revision/recursive_drawing.py
@@ -1,14 +1,28 @@
-def recursive_drawing(idx):
-    if idx < 1:
-        return
-
-    print("*" * idx)
-
-    recursive_drawing(idx - 1)
-
-    print("#" * idx)
+# def recursive_drawing(idx):
+#     if idx < 1:
+#         return
+#
+#     print("*" * idx)
+#
+#     recursive_drawing(idx - 1)
+#
+#     print("#" * idx)
+#
+#
+# number = int(input())
+#
+# recursive_drawing(number)
 
 
 number = int(input())
+temp = number
 
-recursive_drawing(number)
+while temp > 0:
+    print("*" * temp)
+    temp -= 1
+
+temp += 1
+
+while temp <= number:
+    print("#" * temp)
+    temp += 1
