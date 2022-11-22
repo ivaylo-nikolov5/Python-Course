6cd518a519e36b631df100039ea61e4920d5420c
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-11-22 15:33:14 +0200

    Added a iterative solution for the Recursive Array Sum problem

--- Data Structures and Algorithms/Revision/recursive_array_sum.py
+++ Data Structures and Algorithms/Revision/recursive_array_sum.py
@@ -1,13 +1,23 @@
-def recursive_array_sum(array, idx):
-    result = 0
-
-    if idx == len(array):
-        return result
-
-    return array[idx] + recursive_array_sum(array, idx + 1)
+# def recursive_array_sum(array, idx):
+#     result = 0
+#
+#     if idx == len(array):
+#         return result
+#
+#     return array[idx] + recursive_array_sum(array, idx + 1)
+#
+#
+# array = [int(num) for num in input().split()]
+#
+# array_sum = recursive_array_sum(array, 0)
+# print(array_sum)
 
 
 array = [int(num) for num in input().split()]
 
-array_sum = recursive_array_sum(array, 0)
+array_sum = 0
+
+while array:
+    array_sum += array.pop()
+
 print(array_sum)
