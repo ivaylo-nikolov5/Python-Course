58e9ff00ef03e2f2b458c7a54229f53e81b6d8a9
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-11-23 15:37:51 +0200

    Added an iterative solution for the Reverse Array problem

--- Data Structures and Algorithms/Revision/cache.py
+++ Data Structures and Algorithms/Revision/cache.py
@@ -219,3 +219,31 @@ print(calc_fib(number, cache))
 
 # -------------------------------------------------------------------------------------------------------
 
+
+def reverse_array(array, mid, idx):
+    if idx == mid:
+        return array
+
+    array[idx], array[-idx - 1] = array[-idx - 1], array[idx]
+    return reverse_array(array, mid, idx + 1)
+
+
+array = input().split()
+
+mid = len(array) // 2
+
+print(*reverse_array(array, mid, 0))
+
+
+array = input().split()
+reversed_array = []
+while array:
+    reversed_array.append(array.pop())
+
+print(*reversed_array)
+
+
+# ----------------------------------------------------------------------------------------------------
+
+
+
--- Data Structures and Algorithms/Revision/reverse_array.py
+++ Data Structures and Algorithms/Revision/reverse_array.py
@@ -1,13 +1,21 @@
-def reverse_array(array, mid, idx):
-    if idx == mid:
-        return array
-
-    array[idx], array[-idx - 1] = array[-idx - 1], array[idx]
-    return reverse_array(array, mid, idx + 1)
+# def reverse_array(array, mid, idx):
+#     if idx == mid:
+#         return array
+#
+#     array[idx], array[-idx - 1] = array[-idx - 1], array[idx]
+#     return reverse_array(array, mid, idx + 1)
+#
+#
+# array = input().split()
+#
+# mid = len(array) // 2
+#
+# print(*reverse_array(array, mid, 0))
 
 
 array = input().split()
+reversed_array = []
+while array:
+    reversed_array.append(array.pop())
 
-mid = len(array) // 2
-
-print(*reverse_array(array, mid, 0))
+print(*reversed_array)
