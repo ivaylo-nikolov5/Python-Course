e74bd5e5912dd908da1e0f1302e064fa4931b733
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-11-23 21:09:58 +0200

    Added a solution for the Binary Search problem

--- Data Structures and Algorithms/Revision/cache.py
+++ Data Structures and Algorithms/Revision/cache.py
@@ -406,5 +406,35 @@ word_cruncher(0, word, words_by_idx, words_repeat, [])
 # -----------------------------------------------------------------------------------------------------------
 
 
+def binary_search(numbers, target):
+    left = 0
+    right = len(numbers) - 1
+
+    while left <= right:
+        mid_idx = (left + right) // 2
+        mid_el = numbers[mid_idx]
+        if target == mid_el:
+            return mid_idx
+        elif mid_el < target:
+            left = mid_idx + 1
+        else:
+            right = mid_idx - 1
+    return -1
+
+
+numbers = [int(x) for x in input().split()]
+target = int(input())
+
+idx = binary_search(numbers, target)
+print(idx)
+
+idx = binary_search(numbers, target)
+print(idx)
+
+
+# -------------------------------------------------------------------------------------------------------
+
+
+
 
 
