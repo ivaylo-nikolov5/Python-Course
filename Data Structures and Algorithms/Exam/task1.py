19814f3425c6e9168f9118d5fcb1be2775d43f3e
ivaylo-nikolov5 <107492840+ivaylo-nikolov5@users.noreply.github.com>
2022-11-24 13:58:53 +0200

    Failed the exam before it has even started but no problems someday I will take it I promise 😢

---
+++ Data Structures and Algorithms/Exam/task1.py
@@ -0,0 +1,15 @@
+def generate_numbers_sum(number, result):
+    if number <= 1:
+        return
+
+    print()
+
+
+
+
+
+
+number = int(input())
+print(number)
+
+generate_numbers_sum(number - 1, [])
