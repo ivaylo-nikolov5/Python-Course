e40f0ad18aba5792d9ac6aa4fd19945b33cde12d
Ivaylo <www.ivailo135@gmail.com>
2022-12-01 13:40:03 +0200

    Connected a DB to the project and added a table

--- Python-Web/django101/django101/settings.py
+++ Python-Web/django101/django101/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'django101.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tasks_db',
+        'USER': 'postgres',
+        'PASSWORD': 'qwerty1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
