95d59907fee7cdb0ab8f5a75081cdef17044c2b3
Ivaylo <www.ivailo135@gmail.com>
2022-12-01 15:29:01 +0200

    Added basic HTML

--- Python-Web/django101/django101/tasks/views.py
+++ Python-Web/django101/django101/tasks/views.py
@@ -12,3 +12,14 @@ def show_all_tasks(request):
     all_tasks = Task.objects.order_by("id").all()
     result = ", ".join(f"{t.name}({t.id})" for t in all_tasks)
     return http.HttpResponse(result)
+
+
+def index(request):
+    all_tasks = Task.objects\
+        .order_by("id")\
+        .all()
+    context = {
+        "title": "The tasks app!",
+        "tasks": all_tasks,
+    }
+    return render(request, 'index.html', context)
