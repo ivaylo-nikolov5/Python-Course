a7652dcf6118acea946a9359decc3e2ce14be78a
Ivaylo <www.ivailo135@gmail.com>
2022-12-11 12:56:11 +0200

    Added the new files for the project

--- Python-Web/departments_app/departments101/departments101/asgi.py
+++ Python-Web/departments_app/departments101/departments101/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for departments_app project.
+ASGI config for departments101 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'departments_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'departments101.settings')
 
 application = get_asgi_application()
