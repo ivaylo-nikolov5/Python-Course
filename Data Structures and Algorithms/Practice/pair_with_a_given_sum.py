0c578e80eb5b3db4271780de0ded480533aac25e
Ivaylo <www.ivailo135@gmail.com>
2022-12-21 13:00:57 +0200

    Added a second solution for the "Find a pair with the given sum in an array" problem

--- Data Structures and Algorithms/Practice/pair_with_a_given_sum.py
+++ Data Structures and Algorithms/Practice/pair_with_a_given_sum.py
@@ -15,4 +15,28 @@ def find_pair_with_given_sum(numbers, target):
         print(f"Pair found {pair}")
 
 
-find_pair_with_given_sum([8, 7, 2, 5, 3, 1], 10)
+def solution2(numbers, target):
+    result = []
+    numbers = sorted(numbers)
+
+    start, end = 0, len(numbers) - 1
+
+    while start < end:
+        if numbers[start] + numbers[end] == target:
+            result.append(f"Pair found: ({numbers[start]}, {numbers[end]})")
+            start += 1
+            continue
+
+        if numbers[start] + numbers[end] < target:
+            start += 1
+        else:
+            end -= 1
+
+    if not result:
+        print("Pair not found")
+        return
+
+    for res in result:
+        print(res)
+
+solution2([8, 7, 2, 5, 3, 1], 10)
