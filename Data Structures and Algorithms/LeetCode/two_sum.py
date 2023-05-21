86c09fe635a87d5e8e6aa700c04eb50cc4d10700
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-05-21 23:16:14 +0300

    Added linear and optimized solution for the Two Sum problem

--- Data Structures and Algorithms/LeetCode/two_sum.py
+++ Data Structures and Algorithms/LeetCode/two_sum.py
@@ -1,10 +1,24 @@
-nums = input()[1:-1].split(",")
-nums = [int(x) for x in nums]
+nums = [3,2,4]
+target = 6
 
-target = int(input())
+def linear_solution(nums, target):
+    for i in range(len(nums) - 1):
+        for j in range(1, len(nums)):
+            if nums[i] + nums[j] == target and j != i:
+                print([i, j])
+                break
 
-for i in range(len(nums) - 1):
-    for j in range(1, len(nums)):
-        if nums[i] + nums[j] == target and j != i:
-            print([i, j])
-            break
+def optimized_solution(nums, target):
+    cache = {}
+
+    for idx in range(len(nums)):
+        num = nums[idx]
+        if target - num in cache:
+            return [cache[target - num], idx]
+
+        cache[num] = idx
+
+print(optimized_solution(nums, target))
+
+
+
